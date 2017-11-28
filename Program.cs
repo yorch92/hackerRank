1111034254aabfb7291c6acc84f9e4934725b72c
Jorge Escalera <jorge.escalera@econnect.mx>
2017-11-28 00:18:56 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hackerRank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
